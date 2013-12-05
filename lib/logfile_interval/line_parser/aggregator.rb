1c56149e815c7cadf7e325c30b64cece69e280e9
Philippe Le Rohellec <plerohellec@yammer-inc.com>
2013-12-04 19:21:49 -0800

    Delta must maintain a previous counter by group key

--- lib/logfile_interval/line_parser/aggregator.rb
+++ lib/logfile_interval/line_parser/aggregator.rb
@@ -73,12 +73,17 @@ module LogfileInterval
       end
 
       class Delta < Base
+        def initialize
+          @previous = Counter.new
+          super
+        end
+
         def add(value, group_by = nil)
-          if @previous
-            @val.add(key(group_by), @previous - value)
+          if @previous.has_key?(key(group_by))
+            @val.add(key(group_by), @previous[key(group_by)] - value)
             @size.increment(key(group_by))
           end
-          @previous = value
+          @previous.set(key(group_by), value)
         end
       end
     end
