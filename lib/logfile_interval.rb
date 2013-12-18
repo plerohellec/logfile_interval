5f69344b74b745f52cc902b21ad9cd5b3736a4af
Philippe Le Rohellec <plerohellec@yammer-inc.com>
2013-12-17 21:55:47 -0800

    fix require paths

--- lib/logfile_interval.rb
+++ lib/logfile_interval.rb
@@ -1,12 +1,14 @@
-require "logfile_interval/version"
-require "logfile_interval/file_backward"
-require "logfile_interval/interval"
-require "logfile_interval/interval_builder"
-require "logfile_interval/logfile"
-require "logfile_interval/logfile_set"
-require "logfile_interval/line_parser/base"
-require "logfile_interval/line_parser/aggregator"
-require "logfile_interval/line_parser/counter"
+lib_dir = File.expand_path('..', __FILE__)
+
+require "#{lib_dir}/logfile_interval/version"
+require "#{lib_dir}/logfile_interval/file_backward"
+require "#{lib_dir}/logfile_interval/interval"
+require "#{lib_dir}/logfile_interval/interval_builder"
+require "#{lib_dir}/logfile_interval/logfile"
+require "#{lib_dir}/logfile_interval/logfile_set"
+require "#{lib_dir}/logfile_interval/line_parser/base"
+require "#{lib_dir}/logfile_interval/line_parser/aggregator"
+require "#{lib_dir}/logfile_interval/line_parser/counter"
 
 module LogfileInterval
 end
