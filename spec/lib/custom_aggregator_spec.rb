0dd8c5e8476878e571ee76c54341166d20dd4abe
Philippe Le Rohellec <philippe@lerohellec.com>
2014-01-12 17:30:50 -0800

    add test for set_column_custom_options

--- spec/lib/custom_aggregator_spec.rb
+++ spec/lib/custom_aggregator_spec.rb
@@ -9,18 +9,33 @@ module LogfileInterval
       @line_parser_class = LineParser::CustomTimingLog
     end
 
-    it 'gets instantiated with empty data' do
-      interval = Interval.new(@end_time, @length, @line_parser_class)
+    def fill_interval
+      @interval = Interval.new(@end_time, @length, @line_parser_class)
       record1 = @line_parser_class.create_record('1385942400, 192.168.0.5, posts#index, 150, 20000, 53.0')
-      interval.add_record(record1)
+      @interval.add_record(record1)
       record2 = @line_parser_class.create_record('1385942300, 192.168.0.5, posts#show, 50, 10000, 51.0')
-      interval.add_record(record2)
-      record3 = @line_parser_class.create_record('1385942200, 10.10.10.10, posts#show, 60, 12000, 50.0')
-      interval.add_record(record3)
+      @interval.add_record(record2)
+      record3 = @line_parser_class.create_record('1385942200, 10.10.10.10, posts#show, 70, 12000, 50.0')
+      @interval.add_record(record3)
+    end
 
-      interval.size.should == 3
-      interval[:num_slow].should == 1
-      interval[:ip].should == 3
+    it 'gets instantiated with empty data' do
+      fill_interval
+
+      @interval.size.should == 3
+      @interval[:num_slow].should == 1
+      @interval[:ip].should == 3
+    end
+
+    describe 'set_column_custom_options' do
+      it 'overwrites custom aggregator custom options' do
+        @line_parser_class.set_column_custom_options(:num_slow, :threshold => 60)
+        fill_interval
+
+        @interval.size.should == 3
+        @interval[:num_slow].should == 2
+        @interval[:ip].should == 3
+      end
     end
   end
 end
