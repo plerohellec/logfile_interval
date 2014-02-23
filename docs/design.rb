b580fef98f98e53d3ee4c87718aa37320fdd1300
Philippe Le Rohellec <philippe@lerohellec.com>
2014-02-23 15:03:04 -0800

    prep design for parser class

--- docs/design.rb
+++ docs/design.rb
@@ -1,58 +1,82 @@
 module LogfileInterval
-  module LineParser
+  module Parser
     class Base
       class << self
-        def set_regex(regex)
+        def columns
+          @columns ||= {}
         end
 
-        def add_column(name, options)
+        def set_regex
+        end
+
+        def add_column
           agg = Aggregators.klass(aggregator)
           @columns[name] = { :pos => pos, :aggregator => agg, :conversion => conversion }
           define_method(name)
         end
+      end
 
-        def parse(line)
-          match_data = regex.match(line)
-          data = f(match_data)
-        end
-
-        def create_record(line)
-          record = new(line)
-          return record.valid? ? record : nil
-        end
+      def parse(line)
+        match_data = regex.match(line)
+        data = {}
+        data = f(match_data)
       end
     end
 
-    class AccessLog < Base
+    class AccessLogParser < Base
       set_regex /blah/
       add_column :name => :foo, :pos => 1, :conversion => integer, :aggregator => :average
+    end
+  end
 
-      def initialize(line)
-        @data = self.class.parse(line)
+  class AggregatorSet
+    def initialize(parser_columns)
+      @aggregators = {}
+      parser_columns.each do |name, options|
+        @aggregators[name] = options[:aggregator].new(options)
+      end
+    end
+
+    def add_record(record)
+      @aggregators.each do |name, agg|
+        agg.add_record(record)
+      end
+    end
+
+    def each
+      @aggregators.each do |name, agg|
+        yield name, agg
       end
     end
   end
 
-
   class Interval
-    def initialize(end_time, length, parser)
-      @data = {}
-      parser.columns.each do |name, options|
-        @data[name] = options[:aggregator].new
-      end
+    def initialize(end_time, length, parser_columns)
+      @data = AggregatorSet.new(parser_columns)
     end
 
     def [](name)
       @data[name].value
     end
 
-    def add_record(record)
-      return unless record.valid?
-      raise ParserMismatch unless record.class == parser
-
+    def add(record)
       @size += 1
-      parser.columns.each do |name, options|
-        @data[name].add(record[name])
+      @aggregators.add_record(record)
+    end
+  end
+
+  class IntervalBuilder
+    def initialize(parsed_line_enum, parser_columns, length)
+    end
+
+    def each_interval
+      interval = Interval.new(now, length, parser_columns)
+      parsed_line_enum.each do |record|
+        while record.time < interval.start_time do
+          yield interval
+          interval = Interval.new(interval.start_time, length, aggregators)
+        end
+        interval.add(record)
       end
     end
   end
@@ -90,6 +114,8 @@ module LogfileInterval
     end
 
     def each_line
+      return enum_for(:each_line) unless block_given?
+      ...
     end
 
     def each_parsed_line
@@ -114,23 +140,6 @@ module LogfileInterval
     end
   end
 
-  class IntervalBuilder
-    def initialize(logfile_set, length)
-      parser = logfile_set.parser
-    end
-
-    def each_interval
-      interval = Interval.new(now, length)
-      set.each_parsed_line(parser) do |record|
-        while record.time < interval.start_time do
-          yield interval
-          interval = Interval.new(interval.start_time, length)
-        end
-        interval.add(record)
-      end
-    end
-  end
-
   class Counter < Hash
     def increment(key)
       self[key] = self[key] ? self[key] + 1 : 1
@@ -141,7 +150,7 @@ end
 logfiles = [ 'access.log', 'access.log.1', 'access.log.2' ]
 logfile = logfiles.first
 
-parser = LineParser::AccessLog
+parser = LineParser::AccessLogParser.new
 
 logfile_iterator = LogfileInterval::Logfile.new(logfile, parser)
 logfile_iterator.each_line do |line|
@@ -156,13 +165,13 @@ logfile_iterator.each_parsed_line do |record|
   puts record.time
 end
 
-set_iterator = LogfileInterval::LogfileSet.new(logfiles, parser)
-set_iterator.each_parsed_line do |record|
+set = LogfileInterval::LogfileSet.new(logfiles, parser)
+set.each_parsed_line do |record|
   puts record.class # LineParser::AccessLog
 end
 
 length = 5.minutes
-interval_builder = LogfileInterval::IntervalBuilder.new(logfiles, length)
+interval_builder = LogfileInterval::IntervalBuilder.new(set.each_parsed_line, parser, length)
 interval_builder.each_interval do |interval|
   puts interval.class  # LogfileInterval::Interval
   puts interval.start_time
