47a5be2f01a1093b288d72c9d85125d6e425b424
Philippe Le Rohellec <philippe@lerohellec.com>
2024-07-15 21:19:26 -0700

    class for converting enumerable to enum of parsed lines

---
+++ lib/logfile_interval/model_enum.rb
@@ -0,0 +1,25 @@
+module LogfileInterval
+  class ModelEnum
+    def initialize(models, parser)
+      @models   = models
+      @parser   = parser
+    end
+
+    def each_line
+      return enum_for(:each) unless block_given?
+      @models.each do |line|
+        yield line
+      end
+    end
+
+    def each_parsed_line
+      return enum_for(:each_parsed_line) unless block_given?
+      each_line do |line|
+        record = parser.create_record(line)
+        yield record if record && !record.skip?
+      end
+    end
+    alias_method :each, :each_parsed_line
+  end
+end
+
